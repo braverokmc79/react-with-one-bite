e6ff82c9283c5487e8f1088c6c1a3d5e72a28461
braverokmc79 <braverokmc79@gmail.com>
2022-11-01 19:42:01 +0900

    6.자료형과 형 변환

---
+++ 6/index.js
@@ -0,0 +1,17 @@
+// 템플릿 리터럴
+
+let isSwitchOff = false;
+
+// null 은 의도적으로 선언 
+let a = null;
+
+//값이 없을 경우 기본 undefined
+
+//console.log()
+
+let numberA = 12;
+
+let numberB = "2a";
+
+console.log(numberA * Number(numberB));
+console.log(numberA * parseInt(numberB));
