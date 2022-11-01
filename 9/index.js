a3d572f0435b6003f3fa21040b0c2f9da26a0104
braverokmc79 <braverokmc79@gmail.com>
2022-11-01 20:09:12 +0900

    9.함수

---
+++ 9/index.js
@@ -0,0 +1,12 @@
+let count = 1;//전역 변수
+
+function getArea() {
+    let width = 10;
+    let height = 20;
+
+    let area = width * height;
+    console.log(area);
+}//함수 선언식, 함수 선언 방식의 함수 생성
+
+getArea();
+
