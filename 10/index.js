222e39fe11d0168b7965d238cb7f861f4222440a
braverokmc79 <braverokmc79@gmail.com>
2022-11-01 20:16:10 +0900

    10.함수표현식 & 화살표 함수

---
+++ 10/index.js
@@ -0,0 +1,12 @@
+let hello = () => {
+    return "안녕하세요 여러분1";
+};
+
+const helloText = hello();
+console.log(helloText);
+console.log(helloB());
+
+
+function helloB() {
+    return "안녕하세요. 여러분2";
+}
