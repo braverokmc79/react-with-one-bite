072bbd6de18f4d4beabdf8020e093cb8367fefd9
braverokmc79 <braverokmc79@gmail.com>
2022-11-02 21:46:25 +0900

    18.단락회로 평가

---
+++ 18/index.js
@@ -0,0 +1,10 @@
+console.log(true || false);
+
+const getName = (person) => {
+    const name = person && person.name;
+    return name || "객체가 아닙니다.";
+};
+
+let person = null;
+const name = getName(person);
+console.log(name);
