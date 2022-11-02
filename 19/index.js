967a08d8ea5a58d762c5e7b74f0ced634a1920c9
braverokmc79 <braverokmc79@gmail.com>
2022-11-03 00:57:41 +0900

    19.조건문 업그레이드

---
+++ 19/index.js
@@ -0,0 +1,41 @@
+// function isKoreanFood(food) {
+//   if (food === "불고기" || food === "비빔밥" || food === "떡볶이") {
+//     return true;
+//   }
+//   return false;
+// }
+
+function isKoreanFood(food) {
+    if (["불고기", "비빔밥", "떡볶이"].includes(food)) {
+        return true;
+    }
+    return false;
+}
+
+const food1 = isKoreanFood("불고기");
+const food2 = isKoreanFood("파스타");
+console.log(food1);
+console.log(food2);
+
+const meal = {
+    한식: "불고기",
+    중식: "멘보샤",
+    일식: "초밥",
+    양식: "스테이크",
+    인도식: "카레"
+};
+
+const getMeal = (mealType) => {
+    // if (mealType === "한식") return "불고기";
+    // if (mealType === "양식") return "파스타";
+    // if (mealType === "중식") return "멘보샤";
+    // if (mealType === "일식") return "초밥";
+    // return "굶기";
+
+    //괄호 표기법으로 간단하게
+    return meal[mealType] || "굶기";
+};
+
+console.log(getMeal("한식"));
+console.log(getMeal("중식"));
+console.log(getMeal());
