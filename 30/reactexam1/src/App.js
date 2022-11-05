4fac12ff89836f6fdb7c78e126e771a2c44a82f5
braverokmc79 <braverokmc79@gmail.com>
2022-11-05 14:06:53 +0900

    31.Create React App

---
+++ 30/reactexam1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let name = "최준호";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
