29ed451f831d62ff2458debbd334108ecb9f7adf
braverokmc79 <braverokmc79@gmail.com>
2022-11-05 14:46:38 +0900

    32. JSX

--- 30/reactexam1/src/App.js
+++ 30/reactexam1/src/App.js
@@ -1,14 +1,41 @@
+import React from 'react';
+
 import './App.css';
+import MyFooter from './MyFooter';
+import MyHeader from './MyHeader';
 
 function App() {
-  let name = "최준호";
+  let name = "홍길동";
+
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "green",
+    },
+
+  };
+
+  const number = 5;
+
 
   return (
-    <div className="App">
+
+    // <React.Fragment>
+    //<>
+    <div className="App" style={style.App}>
+      <MyHeader />
       <header className="App-header">
-        <h2>안녕 리액트</h2>
+        <h2 style={style.h2}>안녕 리액트 {name}</h2>
+        <b>
+          {number} 는 {number % 2 === 0 ? "짝수" : "홀수"}
+        </b>
       </header>
     </div>
+    //</>
+    // </React.Fragment> 
+
   );
 }
 
